dad56d7428a23bd9fea92f5c2ff313064176a58b
Draco <drahcirsantos@gmail.com>
2026-07-30 16:01:12 +0800

    minor update

---
+++ sprsdkck-webapp/src/jsx/proximity-hover.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.feature-card').forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    card.style.setProperty('--x', `${x}px`);
+    card.style.setProperty('--y', `${y}px`);
+  });
+});
